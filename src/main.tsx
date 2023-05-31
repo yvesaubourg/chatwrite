4bf5041e1cbef1504c98860e137c49051047323f
Yves Aubourg <yaubourg59@gmail.com>
2023-05-31 09:40:43 -0500

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+
+import {
+  BrowserRouter,
+} from 'react-router-dom'
+
+import App from './app';
+
+const app = createRoot(document.getElementById('root')!);
+
+app.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
